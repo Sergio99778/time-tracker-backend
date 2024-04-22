6da277e846302fe0edae6d36bb71ddea8f9834f7
sergio99778 <sergio4brandon@gmail.com>
2024-04-21 19:29:36 -0600

    Agregadas dependencias de configuración de NestJS y conexión a MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MDatabaseModule } from './mongodb/mdatabase.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.NEST_APP_MONGODB_DATABASE_URL),
+    MDatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
